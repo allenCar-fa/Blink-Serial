644b54a1e64be82c6a06d4a54947a275f054aa83
Mahmoud Nagy <mnagy1312@gmail.com>
2017-07-03 03:56:58 +0200

    raspberry pi not tested

---
+++ serialmanager.c
@@ -0,0 +1,62 @@
+#include "serialmanager.h"
+
+
+//For Nanopi M1 Uart 1
+#define PORT "/dev/ttyS1"
+
+// For Raspberry pi
+//#define PORT "/dev/serial0"
+
+struct termios config;
+
+int fd;
+
+
+int setup_serial(int baud_rate, int block){
+    set_baudrate(baud_rate);
+
+    if(block == UNBLOCK)
+	fd = open(PORT,O_RDWR | O_NOCTTY | O_NONBLOCK);
+    else
+	fd = open(PORT,O_RDWR | O_NOCTTY);
+
+    if(fd == -1)
+	return -1;
+    
+    if(tcgetattr(fd,&config) < 0 )
+	return -1;
+
+    config.c_iflag &= ~(IGNBRK | BRKINT | ICRNL |
+                     INLCR | PARMRK | INPCK | ISTRIP | IXON);
+    config.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+    config.c_cflag &= ~(CSIZE | PARENB);
+    config.c_cflag |= CS8;
+    config.c_cc[VMIN]  = 1;
+    config.c_cc[VTIME] = 0;
+
+    if(cfsetispeed(&config, BAUD_RATE) < 0 || cfsetospeed(&config, BAUD_RATE) < 0)
+	return -1;
+
+    if(tcsetattr(fd, TCSAFLUSH, &config) < 0)
+	return -1;
+    tcflush(fd, TCIOFLUSH);
+    return 0;
+}
+
+
+// returns 0 if no characters available and -1 if error occured 
+ssize_t serial_read(char *buff,int buff_size){
+    ssize_t err;
+    err = read(fd,buff,buff_size);
+    
+    return err;
+}
+
+
+//returns -1 if error occured and else for numbered of written characters
+ssize_t serial_write(char *buff,int buff_size){
+    ssize_t err;
+    err = write(fd,buff,buff_size);
+    tcdrain(fd);
+    return err;
+}
---
+++ serialmanager.h
@@ -0,0 +1,24 @@
+#ifndef __SERIAL_MANAGER__
+#define __SERIAL_MANAGER__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int BAUD_RATE;
+
+#define set_baudrate(A) BAUD_RATE = A;
+
+#define BLOCK 1
+#define UNBLOCK 0
+
+int setup_serial(int baud_rate, int block);
+ssize_t serial_read(char *buff,int buff_size);
+ssize_t serial_write(char *buff,int buff_size);
+
+
+#endif
